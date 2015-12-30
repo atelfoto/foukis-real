6159db73eafdc155f315befe544d35dc989ea9b6
philippe <atelfoto@msn.com>
2015-12-30 16:00:53 +0100

    vendors

---
+++ vendors/config.rb
@@ -0,0 +1,19 @@
+require 'bootstrap-sass'
+require 'font-awesome-sass'
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+font_path ="font"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+# Vous pouvez choisir votre style de production favori ici (peut être passé
+# outre via la ligne d'instruction) :
+# output_style :expanded ou :nested ou :compact ou :compressed
+output_style = :nested
+relative_assets=true
+line_comments = false
+# dans le bath compass install compass pour installer
